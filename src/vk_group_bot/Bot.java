e7a37504f9665e5a5689b66a296977b10fb694bd
electronrussia <bm.kot.urs@mail.ru>
2013-05-19 13:28:54 +0400

    First commit

---
+++ src/vk_group_bot/Bot.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vk_group_bot;
+
+/**
+ *
+ * @author electronrussia
+ */
+public class Bot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
