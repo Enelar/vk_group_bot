ce587a9763d5eb275c7405cff27a88b5cfa0fc4e
electronrussia <bm.kot.urs@mail.ru>
2013-05-19 13:49:53 +0400

    Maven

---
+++ src/main/java/ru/mctop/bot/Bot.java
@@ -0,0 +1,13 @@
+package ru.mctop.bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class Bot 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
